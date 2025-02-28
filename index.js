64e3aae0c8fbc561b5783224647c4ed32795776e
Joelho456 <babusama666@gmail.com>
2025-02-27 22:38:32 -0400

    Testando parametros de rotas

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const server = express();
 
+//Query params =?nome=Carlos&idade=22
+
 server.get("/hello", (req, res) => {
-    return res.json({title: "Hello world",
-        message: "Oi meus amigos"
+    const {nome, idade} = req.query;
+
+    return res.json({
+        title: "Hello world",
+        message: `Ola ${nome} tudo certo?`,
+        idade: idade
+    });
+});
+
+
+//Route params = /hello/:nome
+
+server.get("/hello/:nome/:idade", (req, res) => {
+    const { nome } = req.params;
+
+    return res.json({
+        tittle: "Hello World",
+        message: `Ola ${nome} tudo certo?`
     });
 });
 
